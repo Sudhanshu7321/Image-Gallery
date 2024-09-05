f03dfed85eb0e2b66fa0d3e089b41a19965f48af
Sudhanshu7321 <94299395+Sudhanshu7321@users.noreply.github.com>
2024-09-05 10:45:01 +0530

    Add files via upload

---
+++ index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/index.master" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+ <body style="background-color:#ECF2FF;">
+      <!--    Navigation Bar Start -->
+    <nav class="navbar" style="background-color:#3E54AC;">
+        <div class="container-fluid">
+            <a class="navbar-brand ms-5" style="color:white;font-family:'Times';font-size:30px;" href="#">Image Gallery</a
+        </div>
+    </nav>
+       <!--    Navigation Bar End -->
+
+       <!--    Thumbnail Start -->
+     <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    <div class="container pt-5"><br />
+        <div class="container-fluid">
+            <!-- Image grid Dynamically -->
+            <div runat="server" id="photos"></div>
+        </div>
+    </div>
+       <!--    Thumbnail End -->
+   
+</body>
+</asp:Content>
+
---
+++ index.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{    
+    //Established Database Connection
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Conn"].ConnectionString);
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      Photos();
+    }
+
+  //  Function For Fetch Images From DataBase
+    protected void Photos()
+    {
+        try
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+
+            string sql_query = "select * from gallery";
+            SqlCommand cmd = new SqlCommand(sql_query, con);
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            if (dt.Rows.Count > 0 && dt != null)
+            {
+                string col = "<div class='row'>";
+                for (int i = 0; i < dt.Rows.Count; i++)
+                    col+="<div class='col-md-2 pb-4'> <div class='shadow'><a href='"+ dt.Rows[i]["image"].ToString() + "' data-lightbox='example-set' ><img style='height: 130px;'  class='img-fluid thumbnail' src='"+ dt.Rows[i]["image"].ToString() + "'></a></div> </div>";
+                col += "</div>";
+                photos.InnerHtml = col;
+
+            }
+            else
+                photos.InnerHtml = "Server Down Try Again..";
+
+        }
+        catch (Exception ex)
+        {
+            Label1.Text = ex.Message;
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+   
+}
